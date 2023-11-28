cd1a745e16da1e328341ecde34b99be672c69897
anatolik-bro <anatolik.kasyanov@gmail.com>
2023-11-28 21:48:38 +0500

    добавил формы регистрации и входа

---
+++ src/js/tabs.js
@@ -0,0 +1,23 @@
+const tabsBtn = document.querySelectorAll(".tabs-nav-btn");
+const tabsItems = document.querySelectorAll(".tabs-item");
+
+tabsBtn.forEach(function(item) {
+    item.addEventListener("click", function(){
+        let currentBtn = item;
+        let tabID = currentBtn.getAttribute("data-tab");
+        let currentTab = document.querySelector(tabID);
+
+        if(!currentBtn.classList.contains('active')){
+            tabsBtn.forEach(function(item){
+                item.classList.remove('active');
+            });
+    
+            tabsItems.forEach(function(item){
+                item.classList.remove('active');
+            });
+    
+            currentBtn.classList.add('active');
+            currentTab.classList.add('active');
+        }
+    });
+});
